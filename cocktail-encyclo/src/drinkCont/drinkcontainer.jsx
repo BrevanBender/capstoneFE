22cc8160cf0d7d747686306e2ef973d4d13d1452
BrevanBender <bendercoding@gmail.com>
2022-04-27 16:32:57 -0700

    individual show figured out

--- cocktail-encyclo/src/drinkCont/drinkcontainer.jsx
+++ cocktail-encyclo/src/drinkCont/drinkcontainer.jsx
@@ -4,13 +4,24 @@ import IndividualDrink from "./indiDrink/indiDrink"
 
 const DrinkContainer = ()=>{
     const[showAll, setShow] = useState(true)
+    const[showDrink, setShowDrink] = useState([])
+    const drinkPage = (idx)=>{
+        console.log(idx)
+        console.log(drinks[idx])
+        setShowDrink([
+            drinks[idx]
+        ])
+    }
+    const indexPage =()=>{
+        setShowDrink([])
+    }
     return(
         <div>
             <h1>hey</h1>
-            {showAll?
-                drinks.map((drink)=>{
+            {showDrink.length === 0?
+                drinks.map((drink, index)=>{
                     return(
-                        <div className="ogdrink">
+                        <div className="ogdrink" onClick={()=>{drinkPage(index)}}>
                             <h2>{drink.strDrink}</h2>
                             <img src={drink.strDrinkThumb} className="drinkImage"></img>
                     
@@ -18,7 +29,7 @@ const DrinkContainer = ()=>{
                     )
                 })
             :
-                <IndividualDrink></IndividualDrink>
+                <IndividualDrink drink={showDrink[0]}></IndividualDrink>
             }
         </div>
     )
--- cocktail-encyclo/src/drinkCont/indiDrink/indiDrink.jsx
+++ cocktail-encyclo/src/drinkCont/indiDrink/indiDrink.jsx
@@ -1,8 +1,12 @@
 
 
-const IndividualDrink = ()=>{
+const IndividualDrink = (props)=>{
+    console.log(props.drink)
     return(
-        <h2>Individual drink</h2>
+        <div>
+            <h2>{props.drink.strDrink}</h2>
+            <img src={props.drink.strDrinkThumb} className="drinkImage"></img>
+        </div>
     )
 }
 export default IndividualDrink
