c9a1ab49337cb72679bb2dad316d32f90a4e5c15
BrevanBender <bendercoding@gmail.com>
2022-05-01 20:56:52 -0700

    search by name

--- cocktail-encyclo/src/drinkCont/drinkcontainer.jsx
+++ cocktail-encyclo/src/drinkCont/drinkcontainer.jsx
@@ -34,6 +34,7 @@ const DrinkContainer = ()=>{
             trialarr.push(convertDrink)
         })
         setDesiredDrink(trialarr)
+        console.log(trialarr)
     }
     const getRandoDrink = ()=>{
         let num = Math.floor(Math.random()*547 + 1)
@@ -46,26 +47,32 @@ const DrinkContainer = ()=>{
         setShowDrink([])
     }
     
-    useEffect(convertDrinks, [])
     const updateFilter = (e)=>{
+        console.log(`drink ==== ${desiredDrink}`)
         const filterArray = desiredDrink.filter((drink)=>{
             let i = 0
             drink.ingredients.forEach((ing)=>{
                 if(ing){
-                if(ing.includes(e.target.value)){
-                    i++
-                }
-            }})
-            if(i){return drink}
-        })
-        console.log(filterArray)
-        setDesiredDrink(filterArray)
-    }
+                    if(ing.includes(e.target.value)){
+                        i++
+                    }
+                }})
+                if(i){return drink}
+            })
+            console.log(filterArray)
+            setDesiredDrink(filterArray)
+        }
+        const updateNameFilter = (e)=>{
+            const filterArray = desiredDrink.filter(drink=>drink.strDrink.toString().toLowerCase().includes(e.target.value.toLowerCase()))
+            console.log(filterArray)
+            setDesiredDrink(filterArray)
+        }
+    useEffect(convertDrinks, [])
     return(
         <div>
             {showDrink.length === 0?
             <div>
-                <input type='text' onChange={updateFilter} placeholder='Filter By Ingredient'></input><button onClick={convertDrinks}>All Drinks</button>
+                <input type='text' onChange={updateNameFilter} placeholder='Filter By Name'></input><input type='text' onChange={updateFilter} placeholder='Filter By Ingredient'></input><button onClick={convertDrinks}>All Drinks</button>
                 <button onClick={getRandoDrink}>Get Random Drink</button>
                 <div className="index">
                     {desiredDrink.map((drink, index)=>{
